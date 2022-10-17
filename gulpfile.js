c131cd7d2df213b2c55ebbdda332cbc69412dcad
mazlum <ia.mazlumyildirim@gmail.com>
2022-10-17 14:17:22 +0300

    Fixed all sizes of intro player

--- gulpfile.js
+++ gulpfile.js
@@ -6,19 +6,11 @@ const sass = require('gulp-sass')(require('sass'));
 const { watch, series } = require('gulp');
 
 function style() {
-  return gulp.src('./assets/sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./assets/css'));
+    return gulp.src('./assets/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./assets/css'));
 }
 
-// function javascript(cb) {
-//   // body omitted
-//   cb();
-// }
-
 exports.default = function() {
-  // You can use a single task
-  watch('./assets/sass/**/*.scss', style);
-  // Or a composed task
-  // watch('./assets/script/*.js', series(style, javascript));
+    watch('./assets/sass/**/*.scss', style);
 };
